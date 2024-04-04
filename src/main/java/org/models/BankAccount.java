722f89fea2ed6119f3a87b1732617ffee2de6c8d
senuravihanjayadeva <senurajayadeva@gmail.com>
2024-04-04 20:10:38 +0530

    Implemented Bank Account class

---
+++ src/main/java/org/models/BankAccount.java
@@ -0,0 +1,40 @@
+package org.models;
+
+public class BankAccount {
+    private String accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    public BankAccount() {
+    }
+
+    public BankAccount(String accountNumber, String accountHolderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+
+    public synchronized double getBalance() {
+        return this.balance;
+    }
+
+    public synchronized void depositAmount(double amount) {
+        this.balance = this.balance + amount;
+        System.out.println("Deposited: " + amount + ", New Balance: " + this.balance);
+        notifyAll();
+    }
+
+    public synchronized void withdrawAmount(double amount) {
+        try{
+            if (this.balance < amount) {
+                    throw new InterruptedException("Insufficient balance");
+            }else{
+                this.balance = this.balance - amount;
+                System.out.println("Withdrew: " + amount + ", New Balance: " + this.balance);
+            }
+        }  catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
