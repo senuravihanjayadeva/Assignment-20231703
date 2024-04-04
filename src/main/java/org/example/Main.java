f5cf59a080518149b98c21fab74078e43990f6af
senuravihanjayadeva <senurajayadeva@gmail.com>
2024-04-04 20:36:26 +0530

    implemented main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import org.models.Bank;
+import org.models.BankAccount;
+import org.models.User;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Bank bank = new Bank();
+        BankAccount account = new BankAccount("123456", "John Doe", 1000);
+        bank.addAccount(account);
+
+        for (int i = 0; i < 10; i++) {
+            Thread clientThread = new Thread(new User(bank, "123456"));
+            clientThread.start();
+        }
     }
 }
