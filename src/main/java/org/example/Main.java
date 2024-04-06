d68aebe5f3ab1d42d2862d0f09a33e6a9311acd9
senuravihanjayadeva <senurajayadeva@gmail.com>
2024-04-06 08:08:34 +0530

    Implemented Thread Groups

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,12 +7,20 @@ import org.models.User;
 public class Main {
     public static void main(String[] args) {
         Bank bank = new Bank();
-        BankAccount account = new BankAccount("123456", "John Doe", 1000);
-        bank.addAccount(account);
+        BankAccount account1 = new BankAccount("123456", "John Doe", 1000);
+        BankAccount account2 = new BankAccount("222286", "John Doe", 1000);
 
-        for (int i = 0; i < 10; i++) {
-            Thread clientThread = new Thread(new User(bank, "123456"));
-            clientThread.start();
+        bank.addAccount(account1);
+        bank.addAccount(account2);
+
+        ThreadGroup regularClients = new ThreadGroup("Regular Clients");
+        ThreadGroup vipClients = new ThreadGroup("VIP Clients");
+        
+        for (int i = 0; i < 5; i++) {
+            new Thread(regularClients, new User(bank, "123456"), "Client " + i).start();
+        }
+        for (int i = 0; i < 5; i++) {
+            new Thread(vipClients, new User(bank, "222286"), "VIP Client " + i).start();
         }
     }
 }
